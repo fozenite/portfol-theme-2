7de86bd837662496a9d512ded6895f62718a6653
Rohit <moza.rohit@gmail.com>
2019-02-09 21:55:08 -0500

    Implemented initial final uploading system via carrierwave for portfolio items

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -65,6 +65,8 @@ class MyPortfoliosController < ApplicationController
     params.require(:my_portfolio).permit(:title, 
                                          :subtitle, 
                                          :body, 
+                                         :main_image,
+                                         :thumb_image,
                                          technologies_attributes: [:name]
                                          )
   end

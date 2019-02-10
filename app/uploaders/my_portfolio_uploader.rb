16aeb965c617d681301889214b1cdf4a62ec35b2
Rohit <moza.rohit@gmail.com>
2019-02-09 23:11:44 -0500

    Finished carrierwave installation

--- app/uploaders/my_portfolio_uploader.rb
+++ app/uploaders/my_portfolio_uploader.rb
@@ -2,7 +2,7 @@
 
 class MyPortfolioUploader < CarrierWave::Uploader::Base
 
-  storage :file
+  storage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"

e3b19e313db2a4cdc4298ae1f203cd185e49da51
Rohit <moza.rohit@gmail.com>
2019-02-19 07:16:14 -0500

    Implemented initial connection to twitter API

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Portfolio
   class Application < Rails::Application
     config.load_defaults 5.1
     config.generators.system_tests = nil
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
